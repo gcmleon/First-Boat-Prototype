e69fc4c025a736275332a152e0637f0ff7e1aa2c
Fernando Cardenas <fernandocardenasm@eduroam-050-207.scc.uni-weimar.de>
2016-01-11 19:17:29 +0100

    Add Timer

---
+++ Assets/scripts/timerGame.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class timerGame : MonoBehaviour {
+
+	float currentTimer = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		currentTimer += Time.deltaTime;
+	}
+
+	void OnGUI(){
+		GUI.Label (new Rect(50, 50, 200, 100), "Time: " + (int)currentTimer);
+	}
+}
