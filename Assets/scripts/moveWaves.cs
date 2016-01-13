4b320ce40a04e366635d00aa13659ec480095044
fernandocardenasm <fernando.cardenasm@gmail.com>
2016-01-13 20:14:52 +0100

    Improved right left wave movement

--- Assets/scripts/moveWaves.cs
+++ Assets/scripts/moveWaves.cs
@@ -11,16 +11,19 @@ public class moveWaves : MonoBehaviour {
 	private float boatSideSpeed;
 	private float boatAccelerationSpeed;
 
-	private Rigidbody rightWaveBody;
-	private Rigidbody leftWaveBody;
+	private float accRight = 0;
+	private float accLeft = 0;
+	private Vector3 initRightPosition;
+	private Vector3 initLeftPosition;
 
 	// Use this for initialization
 	void Start () {
-		rightWaveBody = rightWave.GetComponent<Rigidbody>();
-		leftWaveBody = leftWave.GetComponent<Rigidbody> ();
 
 		boatSideSpeed = boat.GetComponent<boat>().sideSpeed;
 		boatAccelerationSpeed = boat.GetComponent<boat>().accelerateSpeed;
+
+		initRightPosition = leftWave.transform.position;
+		initLeftPosition = rightWave.transform.position;
 	}
 
 	// Update is called once per frame
@@ -32,12 +35,25 @@ public class moveWaves : MonoBehaviour {
 		float v = Input.GetAxis("Vertical");
 
 		if (h > 0) {
-			leftWaveBody.transform.position = new Vector3(boatPosition[0]-offset, leftWaveBody.transform.position[1], boatPosition[2]);
+			//Move leftWave along with the boat
+			accLeft += 0.001f;
+			accRight = 0;
+			leftWave.transform.position = new Vector3(boatPosition[0]-offset, leftWave.transform.position[1]+accLeft, boatPosition[2]);
+			rightWave.transform.position = new Vector3(boatPosition[0]+offset, initRightPosition[1], initRightPosition[2]);
+
 		} else if (h < 0) {
 			//Move rightWave along with the boat
-			rightWave.transform.position = new Vector3(boatPosition[0]+offset, rightWaveBody.transform.position[1], boatPosition[2]);
+			accRight += 0.001f;
+			accLeft = 0;
+			rightWave.transform.position = new Vector3(boatPosition[0]+offset, rightWave.transform.position[1]+accRight, boatPosition[2]);
+			leftWave.transform.position = new Vector3(boatPosition[0]-offset, initLeftPosition[1], initLeftPosition[2]);
+
 		} else {
-			
+			accLeft = 0;
+			accRight = 0;
+			leftWave.transform.position = new Vector3(boatPosition[0]-offset, initLeftPosition[1], initLeftPosition[2]);
+			rightWave.transform.position = new Vector3(boatPosition[0]+offset, initRightPosition[1], initRightPosition[2]);
+
 		}
 
 
